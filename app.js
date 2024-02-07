28930b4739c264db0cee9b837cf72f13cf60caa9
Berry Bash <mubashar.nuru@gmail.com>
2024-02-07 18:18:57 +0100

    week13-p2p

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(3000)
+
+// Import routes for users and products
+const usersRouter = require('./routes/users');
+const productsRouter = require('./routes/products');
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Mount the usersRouter at /api/users
+app.use('/api/users', usersRouter);
+
+// Mount the productsRouter at /api/products
+app.use('/api/products', productsRouter);
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
